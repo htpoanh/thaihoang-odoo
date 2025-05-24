c0cf2cb4be213aadebe31dba5b8a73d02d8bdd74
htpoanh <htpoanh.at@gmail.com>
2025-05-25 01:34:38 +0200

    update

--- customer_loyalty_core1/models/loyalty_history.py
+++ customer_loyalty_core1/models/loyalty_history.py
@@ -4,8 +4,8 @@ class LoyaltyHistory(models.Model):
     _name = 'loyalty.history'
     _description = 'Loyalty Point History'
 
-    partner_id = fields.Many2one('res.partner', string='Customer', required=True)
-    change = fields.Integer(string='Points Change')
-    reason = fields.Char(string='Reason')
-    date = fields.Datetime(string='Date', default=fields.Datetime.now)
-    origin = fields.Char(string='Origin')
+    partner_id = fields.Many2one('res.partner', string="Customer", required=True)
+    change = fields.Integer(string="Point Change", required=True)
+    reason = fields.Char(string="Reason")
+    date = fields.Datetime(string="Date", default=fields.Datetime.now)
+    origin = fields.Char(string="Origin")
